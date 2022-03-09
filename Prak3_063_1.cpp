1657cab70d0a12496b5e269d1291a57084185f75
Irfan Rahmat Firmansyah <90383815+Irfanrahmatf@users.noreply.github.com>
2022-03-09 12:40:19 +0700

    Add files via upload

---
+++ Prak3_063_1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int H(const char*s){
+
+int x = 0;
+for (;*s!='\0';s++)
+++x;
+return x;
+}
+main (void){
+cout<<endl;
+cout<<"  Praktikum 3 Struktur Data\n";
+cout<<endl;
+char String [80];
+cout<<"  Inputlah Sebuah Kata      : ";
+cin.getline(String,100);cout<<endl;
+cout<<"  Jumlah Karakternya Adalah : "<<H(String)<<" Huruf "<<endl;
+}
